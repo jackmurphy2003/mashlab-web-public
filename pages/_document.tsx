65fbc9d8175d10e569365e7065f29feb2d1f4eea
jackmurphy2003 <jackmurphyalamo@gmail.com>
2025-09-26 15:11:11 -0700

    fixed vish error

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="MashLab - DJ Mashup Laboratory" />
+        <meta name="theme-color" content="#0C1022" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
